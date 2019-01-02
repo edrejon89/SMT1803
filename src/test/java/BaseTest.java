bf4d19e5d6a93c7395db9539c227eeedd52e0d32
Eduardo Rejon <erejon@walook.com.mx>
2019-01-02 09:33:10 -0600

    .

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -55,7 +55,6 @@ public class BaseTest {
     }
 
     private boolean isAlertPresent() {
-        try {
             driver.switchTo().alert();
             return true;
         } catch (NoAlertPresentException e) {
