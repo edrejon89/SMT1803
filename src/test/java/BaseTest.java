3c7d61bb097a27dd3b3df70b4e5380acd9578c58
Eduardo Rejon <erejon@walook.com.mx>
2019-01-13 21:51:29 -0600

    first commit in eduard's branch

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -55,6 +55,7 @@ public class BaseTest {
     }
 
     private boolean isAlertPresent() {
+        try {
             driver.switchTo().alert();
             return true;
         } catch (NoAlertPresentException e) {
